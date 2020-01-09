59459b5dd1644b740f8f1f858f8e68e2d08277b5
crucialjun <nich.otieno@gmail.com>
2020-01-09 11:53:14 +0300

    Picasso

--- app/src/main/java/com/example/alexr/wiredbraincoffeeco/LandingActivity.java
+++ app/src/main/java/com/example/alexr/wiredbraincoffeeco/LandingActivity.java
@@ -1,9 +1,14 @@
 package com.example.alexr.wiredbraincoffeeco;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
 
 public class LandingActivity extends AppCompatActivity {
 
@@ -12,6 +17,12 @@ public class LandingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
     }
+
+    ImageView landingLogo = findViewById(R.id.image_logo);
+
+
+    //Picasso.get().load("http:www.alexwolfps.com/images.logo.png").into(landinglogo);
+
 
     public void getStarted(View view){
         Intent intent = new Intent(this, MainActivity.class);
